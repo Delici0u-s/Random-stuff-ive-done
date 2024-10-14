b1d00371db605b95552e6a0226be29c9c8477639
Delici0u-s <76475448+Delici0u-s@users.noreply.github.com>
2024-10-14 15:42:30 +0200

    Create Randint_func_with_tests_and_results.c
    just bored in uni

---
+++ just_Code/C/Randint_func_with_tests_and_results.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//randint function
+int randint(int min, int max){
+    if (min > max) {
+        int temp = min;
+        min = max;
+        max = temp;
+    }
+    return rand() % (max - min + 1) + min;
+}
+
+int main(void){
+    // settable vars
+    int SEED = 1;
+    int repetitions = 25;
+    int genmin = -2;
+    int genmax = 20;
+
+    //other for program
+    int min = genmax;
+    int max = genmin;
+    srand(SEED);
+
+    // innitialize result array
+    int yap = abs(min - max)+1;
+    int result[yap];
+    for (int i = 0; i < yap; i++) {
+        result[i] = 0;
+    }
+
+    // test randoutputresults
+    for (int i = 0; i < repetitions; i++) {
+        int tmp = randint(genmin, genmax);
+        if (tmp < min){
+            min = tmp;
+        }
+        else if (tmp > max){
+            max = tmp;
+        }
+        result[tmp]++;
+    }
+
+    // give every random val:
+    int Getspecific = 1; // 0 if yes
+    //get gighest roll, and how often
+    int maxc[] = {0,0};
+    for (int i = 0; i < yap; i++) {
+        if (result[i] > maxc[0]){
+            maxc[0]=result[i];
+            maxc[1]=i;
+        }
+        if (Getspecific == 0){
+            printf("For %i: %i \n", i, result[i]);
+        }
+    }
+    
+    //all the printouts
+    printf("Given: Min: %i Max: %i, Repetitions: %i, with Seed: %i\n", genmin, genmax, repetitions, SEED);
+    printf("in Generation: Min: %i Max: %i\n", min, max);
+    printf("Highest gen: %i, with count %i\n", maxc[1], maxc[0]);
+    printf("done");
+}
